a3315dec633b4e7e14c9dc35ccbc2154329333fc
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2017-12-10 21:18:46 -0500

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,17 @@
+require_relative './application.rb'
+
 class Item
   attr_accessor :name, :price
+  @@item = []
 
   def initialize(name,price)
     @name = name
     @price = price
+    @@item << self
   end
+
+  def self.item
+    @@item
+  end
+
 end
